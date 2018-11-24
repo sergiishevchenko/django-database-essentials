6e9fd88eee620eafc91958775b1d590697e5892d
sergii <sergiishevchenko017@gmail.com>
2018-11-24 21:17:39 +0300

    first commit

--- example/models.py
+++ example/models.py
@@ -3,3 +3,8 @@ from django.db import models
 
 class Simple(models.Model):
     text = models.CharField(max_length=10)
+    number = models.IntegerField(null=True)
+    url = models.URLField(default='www.example.com')
+
+    def __str__(self):
+        return self.url
