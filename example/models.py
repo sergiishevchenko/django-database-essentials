25b21226f3a6d6dfde47767ab41ba10464507f39
sergii <sergiishevchenko017@gmail.com>
2018-11-25 18:18:06 +0300

    first commit

--- example/models.py
+++ example/models.py
@@ -21,10 +21,32 @@ class NullExample(models.Model):
 class Language(models.Model):
     name = models.CharField(max_length=10)
 
+    def __str__(self):
+        return self.name
+
 
 class Framework(models.Model):
     name = models.CharField(max_length=10)
     language = models.ForeignKey(Language, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+
+class Character(models.Model):
+    name = models.CharField(max_length=10)
+    movies = models.ManyToManyField(Movie)
+
+    def __str__(self):
+        return self.name
+
 
 
 
